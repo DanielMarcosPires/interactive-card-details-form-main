6fbeb71c08bc4f3c5528485b06c6c2457507ed06
Daniel Marcos Pires <89848369+DanielMarcosPires@users.noreply.github.com>
2022-12-12 13:40:25 -0300

    Sistema de segurança reforçado

--- js/script.js
+++ js/script.js
@@ -1,22 +1,39 @@
 const send = document.querySelector('#send')
 
+//-------------------------------------------
+//-------------------------------------------
 send.addEventListener("click",()=>{
+   const manutencao = document.querySelectorAll(".Informacao");
    let dados = document.querySelectorAll(".Informacao")
    for(let i = 0; i < 5 ; i++){
-      let manutencao = document.querySelectorAll(".Informacao");
-      const caracter = manutencao[i].value
-      console.log(caracter)
+      const cartao={
+         nome:()=>{
+            const nome = document.querySelector('.proprietario')
+            nome.innerHTML = manutencao[0].value;
+         },
+         numero:()=>{
+          const numeroDocartao = document.querySelector('.idCartao')
+          numeroDocartao.innerHTML = manutencao[1].value;
+         },
+         dataDevencimento:()=>{
+            const vencimento = document.querySelector('.vencimento')
+            vencimento.innerHTML = manutencao[2].value;
+         },
+         cvc:()=>{
+            const cvc = document.querySelector('.cvc')
+            cvc.innerHTML = manutencao[3].value
+         },
+      }
       
-      let numeroDocartao = document.querySelector('.idCartao')  
-      let nome = document.querySelector('.proprietario')
-      let vencimento = document.querySelector('.vencimento')
-      let cvc = document.querySelector('.cvc')
-
-
-      numeroDocartao.innerHTML = manutencao[1].value
-      nome.innerHTML = manutencao[0].value
-      vencimento.innerHTML = manutencao[3].value
-      cvc.innerHTML = manutencao[4].value
-
+      if(manutencao[0].value != '' && manutencao[1].value != '' && manutencao[2].value != '' &&manutencao[3].value != ''){
+        cartao.nome()
+        cartao.numero()
+        cartao.dataDevencimento()
+        cartao.cvc()
+        break;
+      }else{
+         alert('Preencha todo o formulário')
+         break;
+      }
    }
 })
