298e5ddd3d2460c7c8de383a436826ac37564f80
Daniel Marcos Pires <89848369+DanielMarcosPires@users.noreply.github.com>
2022-12-12 19:21:04 -0300

    Projeto finalizado com sucesso

--- js/script.js
+++ js/script.js
@@ -1,26 +1,41 @@
 const send = document.querySelector('#send')
 
-//-------------------------------------------
-//-------------------------------------------
+const formatCardNumber = (cardNumber) => {
+    let finalCardNumber = "";
+    let groupLen = 4;
+    let totalTimes = cardNumber.length / groupLen;
+    
+    for (let idx = 0; idx < totalTimes; idx++) {
+        finalCardNumber += cardNumber.slice(totalTimes, totalTimes+groupLen) + " ";        
+    }
+
+    return finalCardNumber.slice(0, -1);
+};
+
 send.addEventListener("click",()=>{
    const manutencao = document.querySelectorAll(".Informacao");
    let dados = document.querySelectorAll(".Informacao")
    for(let i = 0; i < 5 ; i++){
       const cartao={
          nome:()=>{
-            const nome = document.querySelector('.proprietario')
+            const nome = document.querySelector('.proprietario');
             nome.innerHTML = manutencao[0].value;
          },
          numero:()=>{
-          const numeroDocartao = document.querySelector('.idCartao')
-          numeroDocartao.innerHTML = manutencao[1].value;
+          const numeroDocartao = document.querySelector('.idCartao');
+          
+         
+
+          const valor = manutencao[1].value
+         
+          numeroDocartao.innerHTML = formatCardNumber(valor);
          },
          dataDevencimento:()=>{
-            const vencimento = document.querySelector('.vencimento')
+            const vencimento = document.querySelector('.vencimento');
             vencimento.innerHTML = manutencao[2].value;
          },
          cvc:()=>{
-            const cvc = document.querySelector('.cvc')
+            const cvc = document.querySelector('.cvc');
             cvc.innerHTML = manutencao[3].value
          },
       }
