36093540d2dd48b8a64c0ca2698748aa74e216dc
Daniel Marcos Pires <89848369+DanielMarcosPires@users.noreply.github.com>
2022-12-12 12:38:22 -0300

    JavaScript

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 const send = document.querySelector('#send')
 
 send.addEventListener("click",()=>{
-   const preenchido = document.querySelectorAll(".nomeInformacao");
-   const substituo = document.querySelector('.formulario')
-   const valor = preenchido.values().next().value
-   console.log(valor)
+   let dados = document.querySelectorAll(".Informacao")
+   for(let i = 0; i < 5 ; i++){
+      let manutencao = document.querySelectorAll(".Informacao");
+      const caracter = manutencao[i].value
+      console.log(caracter)
+      
+      let numeroDocartao = document.querySelector('.idCartao')  
+      let nome = document.querySelector('.proprietario')
+      let vencimento = document.querySelector('.vencimento')
+      let cvc = document.querySelector('.cvc')
 
-  
-   
+
+      numeroDocartao.innerHTML = manutencao[1].value
+      nome.innerHTML = manutencao[0].value
+      vencimento.innerHTML = manutencao[3].value
+      cvc.innerHTML = manutencao[4].value
+
+   }
 })
